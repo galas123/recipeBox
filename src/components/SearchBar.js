987826b54ca8989ef00c901a8036738386d9a882
Elena Fedoseeva <feya@ld.yandex.ru>
2017-05-22 01:00:48 +0300

    refactoring

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,7 +8,7 @@ export default class SearchBar extends Component {
   render() {
     const {onChange}=this.props;
     return <div className="search-bar__wrapper">
-      <Search className="search"/>
+      <Search className="search-bar__icon search"/>
       <input className="search-bar__input" type="search" placeholder="search.." onChange={onChange}/>
     </div>;
   }
